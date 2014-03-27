be6e92bb35aa10da565f9f91cc7fe3436083603d
Will Colton <coltonw@gmail.com>
2014-03-26 23:08:06 -0400

    Added javascript for creating a ballot with my bgg userdata.

---
+++ js/createBallotMain.js
@@ -0,0 +1,32 @@
+// ballot.dust main js
+$(document).ready(function() {
+    
+    $('form').submit(function(event) {
+        var id, ballot = [], reqData = {}, action = 'ballot';
+        event.preventDefault();
+        
+        $(':checked').each(function(i, checkbox) {
+            id = $(checkbox).attr('id');
+            ballot.push({
+                id: id,
+                name: $('#' + id + '-name').val(),
+                thumbnail: $('#' + id + '-thumbnail').val(),
+            });
+        });
+        console.log(ballot);
+        reqData.ballot = ballot;
+        action = $('[name=action]').val();
+        $.ajax({
+            type: "POST",
+            url: action,
+            data: reqData,
+            dataType: "json",
+            success: function(data, textStatus) {
+                if (data.redirect) {
+                    // data.redirect contains the string URL to redirect to
+                    window.location.href = data.redirect;
+                }
+            }
+        });
+    });
+});
